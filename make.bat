812c673188eee46b4d0ad41a223be910bfb503dc
@theY4Kman <theY4Kman@gmail.com>
2012-05-02 10:24:57 -0500

    make now builds archive

--- make.bat
+++ make.bat
@@ -1,1 +1,14 @@
-..\spcomp wintox_bhop.sp && copy /Y wintox_bhop.smx ..\..\plugins
+@echo OFF
+
+..\spcomp wintox_bhop.sp && ^
+copy /Y wintox_bhop.smx ..\..\plugins > NUL && ^
+copy /Y wintox_bhop.smx build\addons\sourcemod\plugins > NUL && ^
+copy /Y wintox_bhop.sp +include build\addons\sourcemod\scripting > NUL
+
+if %ERRORLEVEL% == 0 (
+    echo Successfully built and copied plug-in to sourcemod\plugins
+    pushd build
+    ..\7za a -y -tzip wintox.zip addons > NUL
+    if %ERRORLEVEL% == 0 echo Successfully compressed plugin into build\wintox.zip
+    popd
+)
