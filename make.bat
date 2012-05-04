005d459a59861d1271659f3537f3cd86d41b9501
@theY4Kman <theY4Kman@gmail.com>
2012-05-03 19:16:23 -0500

    Removing dropped weapons; make.bat visibility enhancements

--- make.bat
+++ make.bat
@@ -1,6 +1,6 @@
 @echo OFF
 
-echo Incrementing build number...
+echo ######## Incrementing build number...
 SET build_file=include\version_build.inc
 FOR /f "skip=2 tokens=3,4 delims= " %%d IN (%build_file%) DO (
     SET /A b=%%e
@@ -12,25 +12,24 @@ echo #if !defined(WINTOX_VERSION) >> %build_file%
 echo     #define WINTOX_VERSION "%v:~1% %build%" >> %build_file%
 echo #endif >> %build_file%
 
-echo Commencing build #%build%...
+echo ######## Commencing build #%build%...
 ..\spcomp wintox_bhop.sp && ^
 copy /Y wintox_bhop.smx ..\..\plugins > NUL && ^
 copy /Y wintox_bhop.smx build\addons\sourcemod\plugins > NUL && ^
 copy /Y wintox_bhop.sp +include build\addons\sourcemod\scripting > NUL
 
 if %ERRORLEVEL% == 0 (
-    echo Successfully built and copied plug-in to sourcemod\plugins
+    echo ######## Successfully built and copied plug-in to sourcemod\plugins
     
-    echo Reloading plug-in server-side...
+    echo ######## Reloading plug-in server-side...
     FOR /f "tokens=1 delims=:" %%d IN ('ping %computername% -4 -n 1 ^| find /i "reply"') DO (
         FOR /F "tokens=3 delims= " %%g IN ("%%d") DO set ip=%%g
     )
     clircon -P"marshmallows" -a%ip% "sm plugins unload wintox_bhop;sm plugins load wintox_bhop"
-    echo Plug-in reloaded!
     
-    echo Compressing build archive...
+    echo ######## Compressing build archive...
     pushd build
     ..\7za a -y -tzip wintox.zip addons > NUL
-    if %ERRORLEVEL% == 0 echo Successfully compressed plugin into build\wintox.zip
+    if %ERRORLEVEL% == 0 echo ######## Successfully compressed plugin into build\wintox.zip
     popd
 )
