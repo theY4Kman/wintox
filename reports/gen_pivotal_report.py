4ccda33201d40656bd5016035e7335ff320b5011
@theY4Kman <theY4Kman@gmail.com>
2012-07-04 23:54:26 -0400

    Report generator corrects to Eastern time

--- reports/gen_pivotal_report.py
+++ reports/gen_pivotal_report.py
@@ -1,3 +1,4 @@
+import pytz
 import os
 from datetime import datetime
 from jinja2 import Template
@@ -26,16 +27,29 @@ class CouldNotFindWintoxError(Exception):
     pass
 
 
+def correct_times(block):
+    # Grab our timezone objects
+    tz_utc = pytz.timezone('UTC')
+    tz_eastern = pytz.timezone('America/New_York')
+
+    for story in block:
+        for key in ('created_at', 'updated_at'):
+            story[key] = story[key].replace(tzinfo=tz_utc)
+            story[key] = story[key].astimezone(tz_eastern)
+
+    return block
+
+
 def main(argv):
     if PIVOTAL_TOKEN_ENV_VAR not in os.environ:
         raise KeyError('%s not in environment' % PIVOTAL_TOKEN_ENV_VAR)
 
+    # Log ourselves in using our API token from the env var
     token = os.environ[PIVOTAL_TOKEN_ENV_VAR]
     client = pivotal.PivotalClient(token=token, cache='.cache')
-    projects = client.projects.all()['projects']
 
     wintox = None
-    for project in projects:
+    for project in client.projects.all()['projects']:
         if project['name'].lower().strip() == 'wintox':
             wintox = project['id']
             break
@@ -44,16 +58,20 @@ def main(argv):
         raise CouldNotFindWintoxError('Could not find Wintox in projects list.')
 
     all_stories = client.stories.all(wintox)['stories']
+    # Creates a dict of { state: [story, story2, ...] }
     stories = dict([(name, list(stories)) for name,stories in groupby(all_stories, lambda story: story['current_state'])])
+
     blocks = {}
     for group,statuses in PIVOTAL_STATUS_GROUPS.iteritems():
         if len(statuses) == 1:
-            blocks[group] = stories.get(statuses[0], [])
+            blocks[group] = correct_times(stories.get(statuses[0], []))
             continue
 
         block = []
         for status in statuses:
             block += stories.get(status, [])
+
+        block = correct_times(block)
 
         sorted(block, key=lambda story: story['created_at'], reverse=True)
         blocks[group] = block
